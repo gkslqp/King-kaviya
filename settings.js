1ecb789ba719435fa2dc92e1139b7c9797148f4d
gkslqp <163153697+gkslqp@users.noreply.github.com>
2024-05-26 09:42:03 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,17 +36,17 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
+global.vcardowner = ['94789312357'] //ur owner number
+global.ownername = "𝘬𝓲ꪀᧁ 𝘬ꪖꪜ𝓲ꪗꪖ" //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
+global.socialm = "𝘬𝓲ꪀᧁ 𝘬ꪖꪜ𝓲ꪗꪖ" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
+global.owner = ['94789312357']
+global.ownertag = '94789312357' //ur tag number
+global.botnumber = '94789312357' //Add your Bot Number For Log
+global.botname = 'ꀘꀤꈤꁅ ꀘꍏ꒦ꀎꌩꍏ ꌃꂦ꓄' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
